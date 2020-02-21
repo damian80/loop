4c239777d9d84bb0e2e77f68b82f0e75003b676b
Damian Ciasnocha <daneek80@gmail.com>
2020-02-21 06:19:04 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // podpinamy pod przycisk BUTTON nasluchiwanie na CLICK
 // tworzymy zmienna const dla przycisku BUTTON
 const btn = document.querySelector('button');
+// tworzymy zmienna let dla numerow
+let number = 1;
 // tworzymy zmienna const dla funkcji ktora nazwiemy addElement
 const addElement = function () {
     const div = document.createElement('div');
     document.body.appendChild(div);
+    div.textContent = number;
+    // zwiekszamy o jeden ++
+    number++;
 
 }
 // tworzymy nasluchiwanie
