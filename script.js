0016f01c9147a94929f9a321f9f6ed7efac3331d
Damian Ciasnocha <daneek80@gmail.com>
2020-02-22 06:58:15 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // podpinamy pod przycisk BUTTON nasluchiwanie na CLICK
 // tworzymy zmienna const dla przycisku BUTTON
-const btn = document.querySelector('button');
+const btn = document.querySelector("button");
 // tworzymy zmienna let dla numerow
 let number = 1;
+let activeNumber = 1;
 // tworzymy zmienna const dla funkcji ktora nazwiemy addElement
-const addElement = function () {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    div.textContent = number;
-    // zwiekszamy o jeden ++
-    number++;
+const addElement = function() {
+  const div = document.createElement("div");
+  div.textContent = number;
+  // zwiekszamy o jeden ++
 
-}
+  // podpinamy klase .circle z "%modulo(remainder operator)"
+
+  if (activeNumber == 5) {
+    activeNumber = 0;
+    div.classList.add("circle");
+  }
+  document.body.appendChild(div);
+  number++;
+  activeNumber++;
+};
+
 // tworzymy nasluchiwanie
-btn.addEventListener('click', addElement)
+btn.addEventListener("click", addElement);
